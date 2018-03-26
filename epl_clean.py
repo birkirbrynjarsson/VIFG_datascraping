9061a06f3e7cc136184f0a27388f4d979ca722ce
Birkir Brynjarsson <birkir@Birkirs-MacBook-Air-2.local>
2018-03-26 00:52:20 +0000

    Calculate mean age of team and add to dataframe with season result data

--- epl_clean.py
+++ epl_clean.py
@@ -1,12 +1,22 @@
 import pandas as pd
 
-def get_csv(filename):
+def get_teamAge(filename):
     data = pd.read_csv(filename, index_col=False)
+    teams = []
     for team in data.Team.unique():
         index = data.Team == team
-        teamAge = data[index].sort_values('Appearances', ascending = False)[:15]['SeasonAtTeam'].mean()
-        print(team + ": " + str(teamAge))
+        teamAge = "{0:.2f}".format(data[index].sort_values('Appearances', ascending = False)[:15]['SeasonAtTeam'].mean())
+        # print(team + ": " + str(teamAge))
+        teams.append([team, teamAge])
+    df = pd.DataFrame(teams, columns=['Team', 'Age'])
+    print(df.sort_values('Age', ascending = False))
+    return df
 
+
+def get_results(filename, year):
+    data = pd.read_csv(filename, index_col=False)
+    return data[((data.League == 'EPL') & (data.Season == year))]
+    
 
 def glossary():
     data = pd.DataFrame()
@@ -21,6 +31,8 @@ def glossary():
     arsenal = data['Team'] == 'Arsenal' # Creates a boolean index for selection
     data[arsenal] # Selects/filters with previously made boolean index 
     team_index = data['Team'].str.contains('United') # Boolean index to teams with 'United' in their name
+    for index, row in data.iterrows():
+        print(row)
 
     arsenal = data['Team'] == 'Arsenal'
     data[arsenal].sort_values('Appearances', ascending=False)[:15]['SeasonAtTeam'].mean()
@@ -28,4 +40,14 @@ def glossary():
     data.Team.unique() # Returns array of each unique value in 'Team' column
 
 if __name__ == "__main__":
-    get_csv("epl_data/2016.csv")
+    year = 2010
+    ageDf = get_teamAge(("epl_data/" + str(year) + ".csv"))
+    resultsDf = get_results("epl_data/epl_result.csv", year)
+    for index, row in resultsDf.iterrows():
+        age = ageDf[(ageDf.Team.str.contains(row['Team']))].iloc[0]['Age']
+        resultsDf.at[index, 'TeamAge'] = age
+    print((resultsDf[:10])['TeamAge'].median())
+    print((resultsDf[10:])['TeamAge'].median())
+    print(resultsDf)
+
+    
