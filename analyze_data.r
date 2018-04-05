05e9035aef660bf6a7e6a7d5ad0bf22332ba425a
Birkir Brynjarsson <birkir@Birkirs-MacBook-Air-2.local>
2018-04-05 23:36:12 +0000

    updatus

--- analyze_data.r
+++ analyze_data.r
@@ -1,5 +1,6 @@
 library (tidyverse) 
 library (scales)
+library(gridExtra)
 
 
 epldata <- read_csv('/Users/birkir/Gitbox/scrape/epl-scrape/epl_data/epl_final.csv')
@@ -42,14 +43,60 @@ epldata %>% filter(Season >= 2006) %>%
     # scale_y_continuous(breaks = 10:15)
     scale_y_continuous(lim=c(0,8)) +
     scale_x_reverse( lim=c(20,0))
-cor(epldata$Pos, epldata$TeamAge)
+cor(epldata$Pos, epldata$MeanAge)
 
-epldata %>% filter(Season >= 2006) %>%
+epldata %>% filter(Team == "Manchester United") %>% 
+  ggplot(aes(Season, Pos)) + 
+  geom_point(aes(color=Team))
+  # scale_y_continuous(breaks = 10:15)
+  # scale_y_continuous(lim=c(0,8))
+
+epldata %>% #filter(Season >= 2006) %>%
   ggplot(aes(group=AgePos, Season, Pos)) +
-    geom_line(data = epldata %>% filter(AgePos > 3, AgePos < 18), colour = 'gray') +
-    geom_line(data = epldata %>% filter(AgePos <= 3), aes(color=AgePos)) +
-    geom_line(data = epldata %>% filter(AgePos >= 18), aes(color=AgePos)) +
-    geom_point(data = epldata %>% filter(AgePos <= 3)) +
-    geom_point(data = epldata %>% filter(AgePos >= 18)) +
+    geom_line(data = epldata %>% filter(AgePos > 1, AgePos < 20), colour = 'gray') +
+    geom_line(data = epldata %>% filter(AgePos <= 1), aes(color=AgePos), size = 2) +
+    geom_line(data = epldata %>% filter(AgePos >= 20), aes(color=AgePos), size = 2) +
+    geom_point(data = epldata %>% filter(Team == "Liverpool"), color = 'Red') +
+    #geom_point(data = epldata %>% filter(AgePos <= 10)) +
+    #geom_point(data = epldata %>% filter(AgePos >= 20)) +
     scale_y_reverse() +
     scale_x_continuous(breaks=pretty_breaks())
+
+p1 <- epldata %>% filter(Team == "Arsenal") %>%
+  ggplot(aes(x = Season)) +
+    geom_line(aes(y = Pos)) +
+    scale_y_reverse(breaks=pretty_breaks(), lim=c(20,1))
+
+p2 <- epldata %>% filter(Team == "Arsenal") %>%
+  ggplot(aes(x = Season)) +
+  geom_line(aes(y = MeanAge)) +
+  scale_y_continuous(breaks=pretty_breaks())
+
+grid.arrange(p1, p2)
+
+epldata %>%
+  ggplot(aes(Pos, MeanAge)) + 
+  #geom_point(aes(color=Pos)) +
+  geom_point(aes()) +
+  # facet_wrap(~Season) +
+  # geom_smooth(method=lm) +
+  # scale_y_continuous(breaks = 10:15)
+  scale_y_continuous(lim=c(1,8)) +
+  scale_x_reverse( lim=c(20,0))
+
+epldata %>%
+  ggplot(aes(Pos, MeanAge)) + 
+  #geom_point(aes(color=Pos)) +
+  geom_point(aes()) +
+  # facet_wrap(~Season) +
+  # geom_smooth(method=lm) +
+  # scale_y_continuous(breaks = 10:15)
+  scale_y_continuous(lim=c(1,8)) +
+  scale_x_reverse( lim=c(20,0))
+
+epldata %>%         
+  ggplot(aes(Pos, MeanAge)) +
+  geom_boxplot(aes(group=Pos)) +
+  scale_x_reverse( lim=c(21,0)) +
+  # geom_smooth(method=lm)
+# coord_flip()
--- epl_clean.py
+++ epl_clean.py
@@ -58,7 +58,7 @@ def glossary():
     data.Team.unique() # Returns array of each unique value in 'Team' column
 
 if __name__ == "__main__":
-    years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+    years = [1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
     resultsDf = get_results("epl_data/epl_result.csv")
     for year in years:
         ageDf = get_teamAge(("epl_data/" + str(year) + ".csv"))
